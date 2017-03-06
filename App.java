ae472a26889554df999996a012623e8a636e94f2
mohan621 <mohancse126@gmail.com>
2017-03-06 17:36:26 +0530

    new

--- App.java
+++ App.java
@@ -7,6 +7,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		System.out.println( "\nsucess" );
+		System.out.println( "\nsucess my dear" );
     }
 }
+
